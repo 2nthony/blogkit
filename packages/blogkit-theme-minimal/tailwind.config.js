a4088383f9dece86131f004b93e6e8b9f9ba1ba1
2nthony <ant.hu@outlook.com>
2022-03-01 00:52:47 +0800

    feat(blogkit-theme-minimal): nextra theme

--- packages/blogkit-theme-minimal/tailwind.config.js
+++ packages/blogkit-theme-minimal/tailwind.config.js
@@ -1,7 +1,71 @@
+// Modified from shuding/nextra
+
+const colors = require('tailwindcss/colors')
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ['./src/**/*.{ts,css,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: [
+        'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      ],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      gray: colors.gray,
+      slate: colors.slate,
+      neutral: colors.neutral,
+      red: colors.red,
+      orange: colors.orange,
+      yellow: colors.yellow,
+      prime: colors.blue,
+      green: colors.green,
+      sky: colors.sky,
+      violet: colors.violet,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
+    extend: {
+      typography: (theme) => ({
+        dark: {
+          css: {
+            color: theme('colors.gray[300]'),
+            '[class~="lead"]': { color: theme('colors.gray[400]') },
+            a: { color: theme('colors.gray[100]') },
+            strong: { color: theme('colors.gray[100]') },
+            'ul > li::before': { backgroundColor: theme('colors.gray[700]') },
+            hr: { borderColor: theme('colors.gray[800]') },
+            blockquote: {
+              color: theme('colors.gray[100]'),
+              borderLeftColor: theme('colors.gray[800]'),
+            },
+            h1: { color: theme('colors.gray[100]') },
+            h2: { color: theme('colors.gray[100]') },
+            h3: { color: theme('colors.gray[100]') },
+            h4: { color: theme('colors.gray[100]') },
+            code: { color: theme('colors.gray[100]') },
+            'a code': { color: theme('colors.gray[100]') },
+            pre: {
+              color: theme('colors.gray[200]'),
+              backgroundColor: theme('colors.gray[800]'),
+            },
+            thead: {
+              color: theme('colors.gray[100]'),
+              borderBottomColor: theme('colors.gray[700]'),
+            },
+            'tbody tr': { borderBottomColor: theme('colors.gray[800]') },
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      typography: ['dark'],
+    },
   },
   plugins: [require('@tailwindcss/typography')],
+  darkMode: 'class',
 }
